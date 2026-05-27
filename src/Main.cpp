c156744311919f9ea09ac79ff11c749a339ea7c2
adrianceti <a25110239@ceti.mx>
2026-05-26 21:31:52 -0600

    se agrego main.cpp y motor.cpp

---
+++ src/Main.cpp
@@ -0,0 +1,7 @@
+#include "../include/Juego.hpp"
+
+int main() {
+    Juego juego;
+    juego.run();
+    return 0;
+}
---
+++ src/Motor.cpp
@@ -0,0 +1,52 @@
+#include "../include/Motor.hpp"
+#include <iostream> 
+
+Motor::Motor() {
+    ventana.create(sf::VideoMode({1280, 720}), "Five Nights at Cinepolis - Oficina");
+    ventana.setFramerateLimit(60);
+
+    
+    if (!texturaOficina.loadFromFile("assets/textures/oficina.png")) {
+        std::cerr << "Error: No se pudo cargar assets/textures/oficina.png" << std::endl;
+        
+        spriteOficina = nullptr;
+    } else {
+        
+        spriteOficina = std::make_unique<sf::Sprite>(texturaOficina);
+    }
+}
+
+Motor::~Motor() {
+    
+}
+
+bool Motor::estaAbierto() const {
+    return ventana.isOpen();
+}
+
+void Motor::procesarEventos() {
+    while (const std::optional<sf::Event> evento = ventana.pollEvent()) {
+        if (evento->is<sf::Event::Closed>()) {
+            ventana.close();
+        }
+    }
+}
+
+void Motor::actualizar() {}
+
+void Motor::renderizar() {
+    ventana.clear(sf::Color::Black); 
+    
+    
+    if (spriteOficina) {
+        ventana.draw(*spriteOficina); 
+    }
+    
+    ventana.display(); 
+}
+
+void Motor::ejecutarCiclo() {
+    procesarEventos();
+    actualizar();
+    renderizar();
+}
